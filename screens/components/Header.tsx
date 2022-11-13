88e5e5bdbf19ff34d73746dbf6c057dd1b1c6a3d
Yokesh <yoksyokesh@gmail.com>
2022-11-13 13:38:34 +0530

    Update Header.tsx

--- screens/components/Header.tsx
+++ screens/components/Header.tsx
@@ -9,22 +9,22 @@ import {
 import React from "react";
 
 const Header = () => {
-  const isLargeScreen = useBreakpointValue({ sm: false, md: true });
+  const isLargeScreen = useBreakpointValue({ base: false, md: true });
   console.log(isLargeScreen);
 
   return (
     <HStack
-      space={{ sm: 2, md: 3, lg: 4 }}
+      space={{ base: 2, md: 3, lg: 4 }}
       justifyContent="space-between"
       alignItems={"center"}
-      p={{ sm: 5 }}
+      p={{ base: 5 }}
     >
       <Box borderColor={"#63FBD7"} borderWidth={1.5} p={2}>
         <Text color={"#63FBD7"} fontWeight="bold">
           Y<Text color={"#B4BFDE"}>okesh</Text>
         </Text>
       </Box>
-      <HStack space={{ sm: 2, md: 3, lg: 4 }} alignItems="center">
+      <HStack space={{ base: 2, md: 3, lg: 4 }} alignItems="center">
         <Text color={"#B4BFDE"}>About</Text>
         <Text color={"#B4BFDE"}>Projects</Text>
         <Text color={"#B4BFDE"}>Contact me</Text>
