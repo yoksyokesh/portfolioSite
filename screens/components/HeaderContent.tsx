0af66d3f33bfa85a08b2278a69dd821329249c38
Yokesh <yoksyokesh@gmail.com>
2022-11-13 14:02:28 +0530

    Update HeaderContent.tsx

--- screens/components/HeaderContent.tsx
+++ screens/components/HeaderContent.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import { HStack,Text,Button, Stack } from "native-base";
+import { HStack, Text, Button, Stack } from "native-base";
 
 const HeaderContent = () => {
-  return (<Stack space={{ base: 2, md: 3, lg: 4 }} alignItems="center" direction={{base:'column',md:'row'}}>
-  <Text color={"#B4BFDE"}>About</Text>
-  <Text color={"#B4BFDE"}>Projects</Text>
-  <Text color={"#B4BFDE"}>Contact me</Text>
-  <Button
-    _text={{ color: "#63FBD7" }}
-    bgColor="#0A192F"
-    borderColor={"#63FBD7"}
-    borderWidth={1.5}
-  >
-    Resume
-  </Button>
-</Stack>);
+  return (
+    <Stack
+      space={{ base: 2, md: 3, lg: 4 }}
+      alignItems="center"
+      direction={{ base: "column", md: "row" }}
+    >
+      <Text color={"#B4BFDE"}>About</Text>
+      <Text color={"#B4BFDE"}>Projects</Text>
+      <Text color={"#B4BFDE"}>Contact me</Text>
+      <Button
+        _text={{ color: "#63FBD7" }}
+        bgColor="#0A192F"
+        borderColor={"#63FBD7"}
+        borderWidth={1.5}
+        height={{ base: 8, md: 10 }}
+      >
+        Resume
+      </Button>
+    </Stack>
+  );
 };
 
 export default HeaderContent;
