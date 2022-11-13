9ace08ee9e7cd4b8773bf476b24b4a4465aea88a
Yokesh <yoksyokesh@gmail.com>
2022-11-13 13:34:30 +0530

    Update Header.tsx

--- screens/components/Header.tsx
+++ screens/components/Header.tsx
@@ -1,9 +1,16 @@
-import { HStack, Text, Button, Icon, useBreakpointValue } from "native-base";
+import {
+  HStack,
+  Text,
+  Button,
+  Icon,
+  useBreakpointValue,
+  Box,
+} from "native-base";
 import React from "react";
 
 const Header = () => {
   const isLargeScreen = useBreakpointValue({ sm: false, md: true });
-  console.log(isLargeScreen)
+  console.log(isLargeScreen);
 
   return (
     <HStack
@@ -12,14 +19,23 @@ const Header = () => {
       alignItems={"center"}
       p={{ sm: 5 }}
     >
-      <Text>
-        Y<Text>okesh</Text>
-      </Text>
+      <Box borderColor={"#63FBD7"} borderWidth={1.5} p={2}>
+        <Text color={"#63FBD7"} fontWeight="bold">
+          Y<Text color={"#B4BFDE"}>okesh</Text>
+        </Text>
+      </Box>
       <HStack space={{ sm: 2, md: 3, lg: 4 }} alignItems="center">
         <Text color={"#B4BFDE"}>About</Text>
         <Text color={"#B4BFDE"}>Projects</Text>
         <Text color={"#B4BFDE"}>Contact me</Text>
-        <Button _text={{color: '#63FBD7'}} bgColor='#0A192F' borderColor={'#63FBD7'} borderWidth={1.5}>Resume</Button>
+        <Button
+          _text={{ color: "#63FBD7" }}
+          bgColor="#0A192F"
+          borderColor={"#63FBD7"}
+          borderWidth={1.5}
+        >
+          Resume
+        </Button>
       </HStack>
     </HStack>
   );
