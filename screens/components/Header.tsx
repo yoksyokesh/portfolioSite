26df582bb3ed3fe9fafcaaf8aeac6c89622c28ab
Yokesh <yoksyokesh@gmail.com>
2022-11-13 13:58:49 +0530

    Update Header.tsx

--- screens/components/Header.tsx
+++ screens/components/Header.tsx
@@ -9,29 +9,39 @@ import {
 import React from "react";
 import HeaderContent from "./HeaderContent";
 import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 
 const Header = () => {
   const isLargeScreen = useBreakpointValue({ base: false, md: true });
-  console.log(isLargeScreen);
+  const [menuPressed, setMenuPressed] = useState(false);
 
   return (
-    <HStack
-      space={{ base: 2, md: 3, lg: 4 }}
-      justifyContent="space-between"
-      alignItems={"center"}
-      p={{ base: 5 }}
-    >
-      <Box borderColor={"#63FBD7"} borderWidth={1.5} p={2}>
-        <Text color={"#63FBD7"} fontWeight="bold">
-          Y<Text color={"#B4BFDE"}>okesh</Text>
-        </Text>
-      </Box>
-      {isLargeScreen ? (
-        <HeaderContent />
-      ) : (
-        <Icon as={Feather} name={"menu"} size={8} color="#63FBD7" />
-      )}
-    </HStack>
+    <Box>
+      <HStack
+        space={{ base: 2, md: 3, lg: 4 }}
+        justifyContent="space-between"
+        alignItems={"center"}
+        p={{ base: 5 }}
+      >
+        <Box borderColor={"#63FBD7"} borderWidth={1.5} p={2}>
+          <Text color={"#63FBD7"} fontWeight="bold">
+            Y<Text color={"#B4BFDE"}>okesh</Text>
+          </Text>
+        </Box>
+        {isLargeScreen ? (
+          <HeaderContent />
+        ) : (
+          <Icon
+            as={Feather}
+            name={"menu"}
+            size={8}
+            color="#63FBD7"
+            onPress={() => setMenuPressed(!menuPressed)}
+          />
+        )}
+      </HStack>
+      {menuPressed ? <HeaderContent /> : <></>}
+    </Box>
   );
 };
 
