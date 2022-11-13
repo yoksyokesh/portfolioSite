3c3e5a666c9352757662b58b0c03fc0e771635a8
Yokesh <yoksyokesh@gmail.com>
2022-11-13 17:45:47 +0530

    wip

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import { Box } from "native-base";
+import { Box, VStack, Text } from "native-base";
 import { useWindowDimensions } from "react-native";
 import { Header } from "./components";
 
@@ -7,8 +7,20 @@ const Home = () => {
   const { height } = useWindowDimensions();
 
   return (
-    <Box height={height} bgColor='#0A192F'>
-      <Header />
+    <Box height={height} bgColor="#0A192F" p={{ base: 5 }}>
+      <VStack space={2}>
+        <Header />
+        <VStack alignSelf={"center"} space={{ base: 2, md: 3, lg: 4 }}>
+          <Text color={"#63FBD7"}>Hi, my name is</Text>
+          <Text color={"#CCD6F6"}>Yokesh.</Text>
+          <Text color={"#8892B0"}>I build things for web, android, iOS.</Text>
+          <Text color={"#65708C"}>
+            I’m a software engineer specializing in building (and occasionally
+            designing) exceptional digital experiences. Currently, I’m focused
+            on building accessible, human-centered products.
+          </Text>
+        </VStack>
+      </VStack>
     </Box>
   );
 };
--- screens/components/Header.tsx
+++ screens/components/Header.tsx
@@ -21,7 +21,6 @@ const Header = () => {
         space={{ base: 2, md: 3, lg: 4 }}
         justifyContent="space-between"
         alignItems={"center"}
-        p={{ base: 5 }}
       >
         <Box borderColor={"#63FBD7"} borderWidth={1.5} p={2}>
           <Text color={"#63FBD7"} fontWeight="bold">
