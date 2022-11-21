988f2a19f0a57ad431891318e9383be9ff87ad59
Yokesh <yoksyokesh@gmail.com>
2022-11-21 05:47:00 +0530

    Update Home.jsx

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           <Text color={"#63FBD7"}>Hi, my name is</Text>
           <Text color={"#CCD6F6"}>Yokesh.</Text>
           <Text color={"#8892B0"}>I build things for web, android, iOS.</Text>
-          <Text color={"#65708C"} fontFamily="Proxima Nova" fontWeight={700}>
+          <Text color={"#65708C"} fontFamily="ProximaNova" fontWeight={700}>
             I’m a software engineer specializing in building (and occasionally
             designing) exceptional digital experiences. Currently, I’m focused
             on building accessible, human-centered products.
